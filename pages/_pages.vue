7efb4910b5a5888544f094074f5c652a967718c0
Jorge Castellon <jorge@thoriumdesign.com>
2019-11-12 18:28:09 -0800

    Correct description value

--- pages/_pages.vue
+++ pages/_pages.vue
@@ -59,7 +59,7 @@ export default {
                             "@type": "WebPage",
                             "url": currentPage,
                             "name": singlePage.attributes.title || "Did not work",
-                            "description": singlePage.attributes.desc || "Did not work",
+                            "description": singlePage.attributes.description || "Did not work",
                             "isPartOf": {
                                 "@type": "WebSite",
                                 "@id": url,
